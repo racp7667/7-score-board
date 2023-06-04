fba737b8ab436619dabe35b179ff0df9d6b72ec0
racp7667 <potlurisc615@gmail.com>
2023-06-04 18:59:13 -0500

    Added code files

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeScore = 0;
+let guestScore = 0;
+
+
+function plusOneHome(){
+    homeScore = homeScore + 1
+    document.getElementById("home-score-val").textContent = homeScore 
+}
+
+function plusOneGuest(){
+    guestScore = guestScore + 1
+    document.getElementById("guest-score-val").textContent = guestScore 
+}
+
+function plusThreeHome(){
+    homeScore = homeScore + 3
+    document.getElementById("home-score-val").textContent = homeScore  
+}
+
+function plusThreeGuest(){
+    guestScore = guestScore + 3
+    document.getElementById("guest-score-val").textContent = guestScore  
+}
+
+function plusFiveHome(){
+    homeScore = homeScore + 5
+    document.getElementById("home-score-val").textContent = homeScore  
+}
+
+function plusFiveGuest(){
+    guestScore = guestScore + 5
+    document.getElementById("guest-score-val").textContent = guestScore  
+}
+
+function reset(){
+    document.getElementById("home-score-val").textContent = 0
+    document.getElementById("guest-score-val").textContent = 0  
+}
+
+function start(){
+    document.getElementById("start-msg").textContent = "LET'S THE GAME BEGIN!!!!"
+}
